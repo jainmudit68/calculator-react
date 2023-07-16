ad798df586f4704064e8a5c66a893bcec377af45
jainmudit68 <77058106+jainmudit68@users.noreply.github.com>
2023-07-16 17:03:40 +0530

    added heading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Calculator from './components/Calculator';
+import Heading from './components/Heading';
 
 function App() {
   return (
+    <>
+    <Heading/>
     <Calculator/>
+    <Heading/>
+    </>
   );
 }
 
---
+++ src/components/Heading.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Heading.css'
+
+export default function Heading() {
+  return (
+    <div className='head'>Simple Calculator</div>
+  )
+}
